7e03927b86f21d020fa60aab54e134995f00dd2a
Soncrates <emersoncus@yahoo.com>
2018-10-03 14:22:01 -0400

    Update LinkParser.py

--- LinkParser.py
+++ LinkParser.py
@@ -1,10 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-def trade_spider(url) :
+def _spider(url) :
     source_code = requests.get(url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text)
-    for link in soup.findAll('a', {'class' : 'item-name'}) :
+    for link in soup.findAll('a') :
         href = link.get('href')
         title = link.string
+        yield href, title
+
+def _spider_list(*url_list) :
+    ret = {}
+    for url in url_list :
+        for href, title in _spider(url) :
+            ret[href] = title
+    return ret
+
+def _spider_main(depth, *url_list) :
+    ret = _spider_list(*url_list)
+    iter_list = range(0,4)
+    for iter in iter_list :
+        _ret = _spider_list(*ret.keys())
+        ret.update(_ret)
+    return ret
+
+if __name__ == "__main__" :
+   url_list = [""]
+   ret = _spider_main(5,*url_list)
